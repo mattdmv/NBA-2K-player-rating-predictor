b190bbd487caf3607759c1fb8a9093ced608dbcb
mattdmv <matija.domovic6@gmail.com>
2021-11-22 23:04:37 +0100

    added flask server, calculate button working

--- client/app.js
+++ client/app.js
@@ -4,11 +4,12 @@ function onClickedCalculate() {
     var playerName = document.getElementById("player-name-placeholder");
     var estRating = document.getElementById("predicted-rating-placeholder");
     
-    var url = "http://127.0.0.1:8000/predict_player_rating"
+    var url = "http://127.0.0.1:5000/predict_player_rating"
 
-    $.post(url, {player_name: player}, function(data, status) {
+    $.post(url, {player_name: player.value}, function(data, status) {
         console.log(data.predicted_value);
         estRating.innerHTML = "<h1>" + data.predicted_value.toString() + "</h1>";
+        playerName.innerHTML = "<h1>" + player.value.toString() + "</h1>"
         console.log(status);
     });
 }
---
+++ server/flask_server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import utility
+ 
+app = Flask(__name__)
+
+@app.route('/predict_player_rating', methods=['GET', 'POST'])
+def predict_player_rating():
+    player_name = request.form['player_name']
+
+    response = jsonify({
+        'predicted_value': utility.main_pipeline(player_name)
+        })
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__=="__main__":
+    print("Starting FastAPI Server for NBA2K player rating prediction!")
+    app.run()
+ 
